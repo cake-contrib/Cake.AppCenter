e770d9931e1fa812a96f6eb7fa9d76bc17cc4bc1
Miha Markič <miha@rthand.com>
2020-12-09 18:21:36 +0100

    Adds missing files under publish directory #20

---
+++ src/Cake.AppCenter/Distribute/Groups/Publish/AppCenter.Alias.DistributeGroupsPublish.cs
@@ -0,0 +1,50 @@
+using Cake.Core;
+using Cake.Core.Annotations;
+using System;
+using System.Collections.Generic;
+
+namespace Cake.AppCenter
+{
+	partial class AppCenterAliases
+	{
+         /// <summary>
+	    /// Publish an app file to a group
+		/// </summary>
+		/// <param name="context">The context.</param>
+		/// <param name="settings">The settings.</param>
+        
+		[CakeMethodAlias]
+        [CakeAliasCategory("Distribute")] 
+		public static void AppCenterDistributeGroupsPublish(this ICakeContext context, AppCenterDistributeGroupsPublishSettings settings)
+		{
+			if (context == null)
+			{
+				throw new ArgumentNullException("context");
+			}
+                 var arguments = new string[0];
+            var runner = new GenericRunner<AppCenterDistributeGroupsPublishSettings >(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
+			 runner.Run("distribute groups publish", settings ?? new AppCenterDistributeGroupsPublishSettings(), arguments);
+		}
+
+
+         /// <summary>
+	    /// Publish an app file to a group
+		/// </summary>
+		/// <param name="context">The context.</param>
+		/// <param name="settings">The settings.</param>
+        /// <returns>Output lines.</returns>
+		[CakeMethodAlias]
+        [CakeAliasCategory("Distribute")] 
+		public static IEnumerable<string> AppCenterDistributeGroupsPublishWithResult(this ICakeContext context, AppCenterDistributeGroupsPublishSettings settings)
+		{
+			if (context == null)
+			{
+				throw new ArgumentNullException("context");
+			}
+                 var arguments = new string[0];
+            var runner = new GenericRunner<AppCenterDistributeGroupsPublishSettings >(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
+			return runner.RunWithResult("distribute groups publish", settings ?? new AppCenterDistributeGroupsPublishSettings(), arguments);
+		}
+
+	}
+}
