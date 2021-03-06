91e1baa5579644d593b15e73b4cd8acbf5a28bf9
Miha Markič <miha@rthand.com>
2021-03-06 17:24:57 +0100

    Closes #23 Updates Cake to 1.0.0

--- src/Cake.AppCenter.Tests/Apps/Create/AppCenterAppsCreateFixture.cs
+++ src/Cake.AppCenter.Tests/Apps/Create/AppCenterAppsCreateFixture.cs
@@ -1,40 +1,26 @@
 ﻿using System;
-using Cake.Testing.Fixtures;
-using Cake.Core;
-using Cake.Core.Diagnostics;
 using Cake.AppCenter;
-using Cake.Core.IO;
-using Cake.Core.Tooling;
-using NSubstitute;
+using Cake.Core;
 using Cake.Core.Configuration;
+using Cake.Core.Diagnostics;
+using Cake.Core.IO;
+using Cake.Testing.Fixtures;
 
 namespace Cake.MobilityCenter.Tests.Apps.Create
 {
     public class AppCenterAppsCreateFixture : ToolFixture<AppCenterAppsCreateSettings>, ICakeContext
     {
-        public string Path { get; set; }
-        IFileSystem fileSystem;
-        ICakeEnvironment environment;
-        IFileSystem ICakeContext.FileSystem => fileSystem;
-        ICakeEnvironment ICakeContext.Environment => environment;
+        public string[] Services { get; set; } = new string[0];
+        IFileSystem ICakeContext.FileSystem => FileSystem;
+        ICakeEnvironment ICakeContext.Environment => Environment;
         public ICakeLog Log => Log;
         ICakeArguments ICakeContext.Arguments => throw new NotImplementedException();
         IProcessRunner ICakeContext.ProcessRunner => ProcessRunner;
         public IRegistry Registry => Registry;
         public ICakeDataResolver Data => throw new NotImplementedException();
         ICakeConfiguration ICakeContext.Configuration => throw new NotImplementedException();
-
         public AppCenterAppsCreateFixture(): base("appcenter")
         {
-            Tools = Substitute.For<IToolLocator>();
-            fileSystem = Substitute.For<IFileSystem>();
-            environment = Substitute.For<ICakeEnvironment>();
-            var toolPath = new FilePath("appcenter");
-            var file = Substitute.For<IFile>();
-            file.Exists.Returns(true);
-            fileSystem.GetFile(toolPath).Returns(file);
-            environment.WorkingDirectory.Returns("C:/Temp");
-            Tools.Resolve("appcenter").Returns(toolPath);
             ProcessRunner.Process.SetStandardOutput(new string[] { });
         }
         protected override void RunTool()
